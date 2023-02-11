2bb3db315a6928e99f8d5571748d1c9824c40d8c
Shreya Pandey <42945783+Xhrya@users.noreply.github.com>
2023-02-11 18:56:39 -0500

    commit on 'main'

--- src/OrderHistory.js
+++ src/OrderHistory.js
@@ -1,12 +1,31 @@
+import { Link } from 'react-router-dom';
+
 const OrderHistory = ({items, title, handleDelete}) =>
 {
 return(
     <div className="OrderHistory">
-
+        {items.map(item => (
+        <div className="item-preview" key={item.id} >
+          <Link to={`/items/${item.id}`}>
+            <h2>{ item.title }</h2>
+            <p>Written by { item.author }</p>
+            <button onClick={() => handleDelete(item.id)}>delete item</button>
+          </Link>
+         
         </div>
+      ))}
 
+
+
+
+    </div>
 );
 
 
 }
 export default OrderHistory;
+
+
+
+
+
