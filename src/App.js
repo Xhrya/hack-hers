21f26e5dd009cb7aa24ea2a38432b13361348c41
shreyapandey <shreyadpandey@gmail.com>
2023-02-11 13:17:38 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+        </p>
+
+
+        <a
+          className="App-link"
+          href="https://www2.wakefern.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wakefern
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
