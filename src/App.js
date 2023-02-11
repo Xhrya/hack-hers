8c4bdc605701231b2b182abb3fbcb7a9ebe8a2b6
shreyapandey <shreyadpandey@gmail.com>
2023-02-11 13:05:02 -0500

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
+
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www2.wakefern.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wakefern
         </a>
       </header>
     </div>
